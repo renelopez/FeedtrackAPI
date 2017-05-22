845cf2b8e5b5585d3f7c13017d0d755eead9d817
renelopez <rene.lopez@unosquare.com>
2017-05-21 23:56:41 -0500

    Added New Routes Folder II

---
+++ routes/feedbackRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const buildFeedbackRoutes=(dbInstance)=>{
+    const router = express.Router();
+
+    router.get('/feedbacks',() => {
+
+    });
+};
+
+export default buildFeedbackRoutes;
+
+
+
