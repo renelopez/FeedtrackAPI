695dac343b2dc0a0207e6c5a6e59e4cdf566b9a3
renelopez <rene.lopez@unosquare.com>
2017-05-21 23:56:41 -0500

    Added New Routes Folder

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser';
 import SourceMapSupport from 'source-map-support';
+import feedbackRoutes from './routes/feedbackRoutes';
 SourceMapSupport.install();
 
 const app=express();
